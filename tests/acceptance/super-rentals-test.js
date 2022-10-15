958eb9b504a5c54e030e2bd1e02f10e35f108d4d
paulo-felipe <alparakleto@gmail.com>
2022-10-14 21:59:04 -0300

    tests/acceptance/super-rentals-test.js: Add test for visinting about and gettin in touch urls

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -16,4 +16,35 @@ module('Acceptance | super rentals', function (hooks) {
 
     assert.strictEqual(currentURL(), '/about');
   });
+
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+
+    assert.strictEqual(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /gettin_it_touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('address').hasText('Super Rentals HQ 1212 Test Address Avenue Testington, OR 97233 +1 (503) 555-1212 superrentalsrep@emberjs.com');
+    assert.dom('address a.phone').hasText('+1 (503) 555-1212');
+    assert.dom('address a.phone').hasProperty('href', 'tel:503.555.1212');
+
+    assert.dom('address a.mail').hasText('superrentalsrep@emberjs.com');
+    assert.dom('address a.mail').hasProperty('href', 'mailto:superrentalsrep@emberjs.com');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
 });
